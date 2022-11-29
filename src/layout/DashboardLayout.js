862c86f36a2853128d27300afdac305f030f8b0d
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-11-29 18:31:33 +0600

    Dashboard conditional rendering ok

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../context/Authprovider/AuthContext';
 import useAdmin from '../hooks/useAdmin';
-// import useBuyer from '../hooks/useBuyer';
+import useBuyer from '../hooks/useBuyer';
 import useSeller from '../hooks/useSeller';
 import UseTitle from '../hooks/UseTitle';
 import Header from '../pages/Shared/Header/Header';
@@ -14,7 +14,7 @@ const DashboardLayout = () => {
     const { user } = useContext(AuthContext);
     const [isAdmin] = useAdmin(user?.email)
     const [isSeller] = useSeller(user?.email)
-    // const [isBuyer] = useBuyer(user?.email)
+    const [isBuyer] = useBuyer(user?.email)
 
     // console.log(isAdmin);
     // console.log(isBuyer);
@@ -30,36 +30,32 @@ const DashboardLayout = () => {
                 <div className="drawer-side bg-accent text-xl text-white">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80  ">
-                        {
-                            isAdmin ?
-                                <>
-                                    <li>
-                                        <Link to="/dashboard/allsellers">All Sellers</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/allbuyers">ALL Buyers</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/dashboard/reportedproducts">Reported Products</Link>
-                                    </li>
-                                </>
-                                :
-                                (
-                                    isSeller ?
-                                        <>
-                                            <li><Link to="/dashboard/buyers">My Buyesr</Link></li>
-                                            <li><Link to="/dashboard/addproduct">Add Products</Link></li>
-                                            <li><Link to="/dashboard/myproducts">My Products</Link></li>
-                                        </>
-                                        :
-                                        <>
 
-                                            <li><Link to="/dashboard/orders">My Orders</Link></li>
-                                            <li><Link to="/dashboard/wishlist">Wish List</Link></li>
-                                        </>
-                                )
+                        {
+                            isAdmin &&
+                            <>
+                                <li><Link to="/dashboard/allsellers">All Sellers</Link></li>
+                                <li> <Link to="/dashboard/allbuyers">ALL Buyers</Link></li>
+                                <li><Link to="/dashboard/reportedproducts">Reported Products</Link></li>
+                            </>
                         }
 
+                        {
+                            isSeller &&
+                            <>
+                                <li><Link to="/dashboard/buyers">My Buyesr</Link></li>
+                                <li><Link to="/dashboard/addproduct">Add Products</Link></li>
+                                <li><Link to="/dashboard/myproducts">My Products</Link></li>
+                            </>
+                        }
+
+                        {
+                            isBuyer &&
+                            <>
+                                <li><Link to="/dashboard/orders">My Orders</Link></li>
+                                <li><Link to="/dashboard/wishlist">Wish List</Link></li>
+                            </>
+                        }
                     </ul>
                 </div>
             </div>
