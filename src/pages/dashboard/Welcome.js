2f026cd3c135d4b939629ac336f8eec3245439e2
mdferdousalam <mdferdousalam1989@yahoo.com>
2022-11-29 18:24:30 +0600

    Dashboard welcome page conditional rendering added

--- src/pages/dashboard/Welcome.js
+++ src/pages/dashboard/Welcome.js
@@ -1,9 +1,32 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/Authprovider/AuthContext';
+import useAdmin from '../../hooks/useAdmin';
+import useSeller from '../../hooks/useSeller';
+import UseTitle from '../../hooks/UseTitle';
+import useBuyer from '../../hooks/useBuyer'
 const Welcome = () => {
+    UseTitle('Welcome Dashboard')
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email)
+    const [isSeller] = useSeller(user?.email)
+    const [isBuyer] = useBuyer(user?.email)
     return (
         <div>
-            <h1>Welcome to your Dashboard</h1>
+            {
+                isAdmin && <>
+                    <h1 className='text-4xl text-center text-primary mt-64'>Welcome to your Admin Dashboard</h1>
+                </>
+            }
+            {
+                isSeller && <>
+                    <h1 className='text-4xl text-center text-primary mt-64'>Welcome to your Seller Dashboard</h1>
+                </>
+            }
+            {
+                isBuyer && <>
+                    <h1 className='text-4xl text-center text-primary mt-64'>Welcome to your Buyer Dashboard</h1>
+                </>
+            }
         </div>
     );
 };
